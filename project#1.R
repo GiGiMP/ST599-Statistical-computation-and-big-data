1eb1bffe191f8f398c83ba02a6b24d21eaf1d452
shangjia <shanga.dong@gmail.com>
2014-04-15 13:59:48 -0700

    shangjia

--- project#1.R
+++ project#1.R
@@ -1,13 +1,22 @@
+# problem so far: 1. The housing and the population is in different spreadsheets, 
+# so we can hardly combine the two together. 2. What do we want to compare, the
+# average income with different ratio in the transportation type occupancy? or in
+# the specific income group, how the transportation type varies, and how the specific 
+# transportation type varies from all different income group. 
+# Based 
+
+
+
 library(ggplot2)
 library(dplyr)
 
-download.file("http://www2.census.gov/acs2012_5yr/pums/csv_hdc.zip",
+download.file("http://www2.census.gov/acs2011_5yr/pums/csv_hdc.zip",
               destfile = "data/csv_hdc.zip")
-download.file("http://www2.census.gov/acs2012_5yr/pums/csv_hny.zip",
+download.file("http://www2.census.gov/acs2011_5yr/pums/csv_hny.zip",
               destfile = "data/csv_hny.zip")
-download.file("http://www2.census.gov/acs2012_5yr/pums/csv_pdc.zip",
+download.file("http://www2.census.gov/acs2011_5yr/pums/csv_pdc.zip",
               destfile = "data/csv_pdc.zip")
-download.file("http://www2.census.gov/acs2012_5yr/pums/csv_pny.zip",
+download.file("http://www2.census.gov/acs2011_5yr/pums/csv_pny.zip",
               destfile = "data/csv_pny.zip")
 
 unzip("data/csv_hdc.zip", list = TRUE)
@@ -25,10 +34,14 @@ select(hdc_df, HINCP, INTP, OIP, PAP, SSIP, ADJINC)
 
 hdc_df <- mutate(hdc_df, income = HINCP + ADJINC)
 
+income_type <- group_by(hdc_df, income)
+group_by(income_type, JWTR)
+
 # HINCP, INTP, OIP, PAP, SSIP, ADJINC
 
 # JWMNP: Travel time to work; JWRIP: Vehicle occupancy: JWTR: means of transportation to work
-
+which(names(hdc_df) == "WAGP")
+which(names(hdc_df) %in% c("WAGP", "COW"))
 
 #==============================================================================
 
@@ -36,7 +49,7 @@ hny <- read.csv(unz("data/csv_hny.zip", "ss12hny.csv"),  # notice just 10 lines!
                   stringsAsFactors = FALSE)
 str(hny)
 #==============================================================================
-pdc <- read.csv(unz("data/csv_pdc.zip", "ss12pdc.csv"),  # notice just 10 lines! why?
+pdc <- read.csv(unz("data/csv_pdc.zip", "ss11pdc.csv"),  # notice just 10 lines! why?
                   stringsAsFactors = FALSE)
 str(pdc)
 
@@ -56,10 +69,14 @@ JWTR_codes <- c("1" = "Car, truck, or van",
                 "10" = "Walked",
                 "11" = "Worked at home",
                 "12" = "Other method")
-pdc_df <- mutate(pdc_df, transport_type = JWTR_codes[JWTR])
+pdc_df <- mutate(pdc_df, transport_type = JWTR_codes[as.character(JWTR)])
+mutate(pdc_df,  Income = WAGP)
+hist(pdc_df$WAGP)
 
 # group by type of transport and summarise
 trans_type <- group_by(pdc_df, JWTR)
+Income_type <- group_by(pdc_df, function(WAGP))
+
 time_summary <- summarise(trans_type, 
                           transport_type = first(transport_type),
                           avg_time = mean(JWMNP, na.rm = TRUE),
@@ -68,7 +85,46 @@ time_summary <- summarise(trans_type,
                           n_missing = sum(is.na(JWMNP)))
 mutate(time_summary, prop = n/sum(n))
 
+# playing with plotting
+time_summary2 <- mutate(time_summary, prop = n/sum(n))
 
+qplot(transport_type, prop, data =time_summary2) 
+qplot(reorder(transport_type, prop), prop, data =time_summary2) 
+qplot( prop, reorder(transport_type, prop), data =time_summary2) 
+
+# don't care about NAs in this plot
+time_summary3 <- mutate(filter(time_summary, !is.na(transport_type)), prop = n/sum(n))
+qplot( prop, reorder(transport_type, prop), data =time_summary3) 
+
+# ok, really I want this state by state, see 03-mode-by-state.r
+
+# TODO:
+#  - look into distributions, shape, spread etc. boxplots, 5 number summary
+#  - is only 56% reasonable for number of workers or are there other reasons this is missing?
+#  - split car/truck/van users by number of people in vehicle (JWRIP)
+#  - how do these numbers vary by state? year?
+
+# === How does the time to commute vary for people === #
+#        that commute via different methods? 
+
+# look at some plots, but just use a sample to keep things quick
+samp <- sample(nrow(or_df), size = 1000)
+
+qplot(JWMNP, data = or_df[samp, ])
+qplot(WAGP, data = or_df[samp, ])
+
+summarise(or_df, mean(is.na(WAGP)), mean(WAGP == 0, na.rm = TRUE))
+# 17% missing,  42% of non-missing earn $0
+
+qplot(log(WAGP), JWMNP, data = or_df[samp, ])
+qplot(JWMNP, log(WAGP), data = or_df[samp, ])
+
+qplot(factor(JWMNP), log(WAGP), data = or_df[samp, ], geom = "boxplot")
+
+qplot(factor(JWMNP), log(WAGP), data = or_df, geom = "boxplot")
+# slight positive relationship between income and mean/median? commute time for commute
+# times below 15 minutes? think about better plot
+# worry about $0 earners
 #==============================================================================
 
 pny <- read.csv(unz("data/csv_pny.zip", "ss12pny.csv"), nrows = 10,  # notice just 10 lines! why?
